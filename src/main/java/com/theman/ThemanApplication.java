0c43898a6084f74c38ec7666479a162086564571
DESKTOP-3C35ERR\kitew <kitewinpsh@naver.com>
2021-11-12 19:25:18 +0900

    aws용 설정 변경

--- src/main/java/com/theman/ThemanApplication.java
+++ src/main/java/com/theman/ThemanApplication.java
@@ -2,11 +2,12 @@ package com.theman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class ThemanApplication {
+public class ThemanApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThemanApplication.class, args);
